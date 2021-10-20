4108134d86ac15b937250ce43fe6072c71619a87
Stefan <stefan.ebmeyer07@gmail.com>
2021-10-20 11:12:32 +0200

    bugfix in setup.py

--- setup.py
+++ setup.py
@@ -3,15 +3,15 @@ import pathlib
 
 setup(
 	name='genview',
-	version'=1.0'
-	description='Gene-centric visualization tool for genomic sequences'
-	url: 'https://github.com/EbmeyerSt/GEnView'
-	author: 'Stefan Ebmeyer'
+	version'=1.0',
+	description='Gene-centric visualization tool for genomic sequences',
+	url: 'https://github.com/EbmeyerSt/GEnView',
+	author: 'Stefan Ebmeyer',
 
 	Classifiers=[
 		'Development Status :: 3 - Alpha',
 		'License :: OSI Approved :: GLP License',
-		'Programming Language :: Python :: 3.6'
+		'Programming Language :: Python :: 3.6',
 		],
 		packages=find_packages(),
 		include_package_data=True,
