244cb6e92a081bc97d33c397a1bc582a53e5ca1b
Stefan <stefan.ebmeyer07@gmail.com>
2021-10-20 13:33:27 +0200

    bugfixes in setup.py

--- setup.py
+++ setup.py
@@ -8,16 +8,11 @@ setup(
 	url='https://github.com/EbmeyerSt/GEnView',
 	author='Stefan Ebmeyer',
 
-	Classifiers=[
-		'Development Status :: 3 - Alpha',
-		'License :: OSI Approved :: GLP License',
-		'Programming Language :: Python :: 3.6',
-		],
-		packages=find_packages(),
-		include_package_data=True,
-		entry_points={
-			'console_scripts': [
-				'create_db=genview_create_db:main',
-				'visualize=genview_visualize:main'],
+	packages=find_packages(),
+	include_package_data=True,
+	entry_points={
+		'console_scripts': [
+			'genview create_db=genview_create_db:main',
+			'genview visualize=genview_visualize:main'],
 			},
 	zip_safe=False)
